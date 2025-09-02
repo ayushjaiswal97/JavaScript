b28962f425ec7736ac32b65b4e89c08b97463083
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-09-02 23:20:44 +0530

    Solve problem on loop

---
+++ Day2/01.js
@@ -0,0 +1,28 @@
+// Print numbers from 1 to 10.
+for(let i = 1; i <= 10; i++){
+    // console.log(i);
+}
+
+// Print numbers from 10 to 1.
+for(let i = 10; i >= 1; i--){
+    // console.log(i);
+}
+
+// Print the multiplication table of 3.
+let mul = 3;
+for(let i = 1; i <= 10; i++){
+    // console.log(`${mul} * ${i} = ${mul*i}`);
+}
+
+// Print the squares of numbers from 1 to 10.
+let sqr = 1;
+for(let i = 1; i<=10; i++){
+    // console.log(`${i} : ${i*i}`);
+}
+
+// Print only even numbers from 1 to 20.
+for(let i = 1; i <= 20; i++){
+    if(i%2===0){
+     console.log(i);
+    }
+}
