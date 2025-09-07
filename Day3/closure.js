28c6bb1445ea7b93f184d725eb1d8e879cfe2d48
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-09-07 21:30:23 +0530

    Add closure examples and variable operations to practice file

--- Day3/closure.js
+++ Day3/closure.js
@@ -6,3 +6,16 @@ function x(){
     y();
 }
 x();
+
+
+function counter(){
+    var counter = 0;
+    return function(){
+        counter++;
+        console.log(counter);
+    }
+}
+var c = counter();
+c();
+c();
+c();
---
+++ Practice/variable.js
@@ -0,0 +1,37 @@
+/*
+Create and Print a Variable
+Create a variable called name and store your name in it.
+Print it using console.log.
+*/
+let name = "Ayush Jaiswal";
+console.log(name);
+
+/*
+Create two variables: num1 = 10 and num2 = 5.
+Print their sum, difference, product, and division.
+*/
+let num1 = 10; 
+let num2 = 5;
+
+let sum = 10 + 5;
+console.log(sum);
+
+let difference = 10 - 5;
+console.log(difference);
+
+let product = 10 * 5;
+console.log(product);
+
+let division = 10 / 5;
+console.log(division);
+
+/*
+Create a variable age = 20.
+Create another variable message = "I am ".
+Combine them into one sentence like: "I am 20".
+ */
+
+const age = 20;
+let message = "I am"
+let sentence = age + message;
+console.log(sentence);
